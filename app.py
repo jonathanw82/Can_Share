6bf52769650aea3e060e127bc1e358e611a272c6
jonathanw82 <jonwhewaycode@outlook.com>
2020-04-03 13:41:46 +0000

    added favicon icon and the first link to the landing page

--- app.py
+++ app.py
@@ -15,3 +15,15 @@ app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
 mongo = PyMongo(app)
 
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("beerceller.html",
+                           tasks=mongo.db.cansAndBottleInfo.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
